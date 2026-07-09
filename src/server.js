c8157d296e834603182e2fe00f96236cb44c3e92
Finn Schmidt <finn.schmidt@brainworxx.de>
2026-07-09 20:52:25 +0200

    änderung

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 3000;
 const router = require("./routes/indexRouter.js");
 
 app.set("view engine", "ejs");
-app.use("/assets", express.static(path.join(__dirname, "../assets")));
+app.use("/assets", express.static(path.join(__dirname, "assets")));
 
 app.use(express.json());
 
